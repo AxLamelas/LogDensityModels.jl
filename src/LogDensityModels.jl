5c6f2d2959d802d86c77c5534cb931c13f3a8478
Afonsso Lamelas <alamelas@ua.pt>
2026-01-22 23:14:48 +0000

    Improved type stability for  logdensity  of DistributionModel when using a ProductNamedTupleDistribution

--- src/LogDensityModels.jl
+++ src/LogDensityModels.jl
@@ -22,13 +22,23 @@ struct DistributionModel{D}
 end
 
 function LD.logdensity(m::DistributionModel,x)
-  T = eltype(x)
-  if !insupport(m.dist,x) 
+  T = flat_eltype(x)
+  if !insupport(m.dist,x)
     return typemin(T)
   end
   # Type assert to deal with type instability in Distributions.jl
   return logpdf(m.dist,x)::T
 end
+
+function LD.logdensity(m::DistributionModel,x::AbstractVector)
+  T = eltype(x)
+  if !insupport(m.dist,x)
+    return typemin(T)
+  end
+  # Type assert to deal with type instability in Distributions.jl
+  return logpdf(m.dist,x)::T
+end
+
 LD.dimension(m::DistributionModel) = length(m.dist)
 LD.capabilities(::Type{<:DistributionModel}) = LD.LogDensityOrder{0}()
 
